f7e9479f40e6808378be1e4f4c6f6d8831c66a89
Yash Aryan <yasharyan307@outlook.com>
2021-06-26 02:10:15 +0530

    syntax correction

--- pages/news/category/[categoryslug]/[pageslug].js
+++ pages/news/category/[categoryslug]/[pageslug].js
@@ -156,7 +156,7 @@ export const getStaticPaths = async () => {
   }
   return {
     paths,
-    fallback: blocking,
+    fallback: 'blocking',
   };
 };
 
--- pages/news/latest/[id].js
+++ pages/news/latest/[id].js
@@ -157,7 +157,7 @@ export const getStaticPaths = async () => {
   }
   return {
     paths,
-    fallback: blocking,
+    fallback: 'blocking',
   };
 };
 
